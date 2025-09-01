b56dcc3e6ae7da10f966a308fb35090d7e5902db
MidnightBlue <113545024+R1fty0@users.noreply.github.com>
2025-08-31 23:23:44 -0700

    fix: dashes not recharging when fully deplayed, working on adding a recharge delay

--- scenes/player/dash/dash_controller.gd
+++ scenes/player/dash/dash_controller.gd
@@ -7,23 +7,28 @@ signal end_dash
 ## How long does a single dash last for. 
 @export var dash_duration: float = 0.02
 ## How long does it take for each dash to recharge 
-@export var dash_cooldown: float = 1.0
+@export var dash_recharge_time: float = 1.0
 ## How many potenial dashes can the player have at one time. 
 @export var max_dash_count: int = 3
+## How long after the last dash does recharging start. 
+@export var dash_recharge_delay: float = 0.30
 
 ## References
 @onready var player: CharacterBody3D = $".."
+## Timer that manages how long dashes take to recharge (yes I know the naming is stupid, should be recharge). 
 @onready var dash_cooldown_timer: Timer = $DashCooldownTimer
 @onready var dash_duration_timer: Timer = $DashDurationTimer
+@onready var dash_recharge_delay_timer: Timer = $DashRechargeDelayTimer
 
 var current_dash_count: int = 3
 var can_dash: bool = true
+var can_recharge_dash: bool = true
 
 func _ready() -> void:
-	dash_cooldown_timer.wait_time = dash_cooldown
+	dash_cooldown_timer.wait_time = dash_recharge_time
 	dash_duration_timer.wait_time = dash_duration
+	dash_recharge_delay_timer.wait_time = dash_recharge_delay
 	current_dash_count = max_dash_count
-	
 
 func _input(event: InputEvent) -> void:
 	# Prevent the player from dashing if they are out of dashes. 
@@ -33,14 +38,30 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("dash") and can_dash:
 		start_dash.emit()
 		current_dash_count -= 1
-		dash_duration_timer.start()
+		# Reset the recharge dash timer and start the recharge delay timer 
+		# if the player uses dash before a dash fully recharges. 
+		if !dash_cooldown_timer.is_stopped():
+			# Start the recharge delay timer.
+			dash_recharge_delay_timer.start()
+			can_recharge_dash = false
+			# Reset the recharge dash timer
+			dash_cooldown_timer.wait_time = dash_recharge_time
+		else:
+			dash_recharge_delay_timer.start()
+			can_recharge_dash = false
 		can_dash = false
 
 func _on_dash_cooldown_timer_timeout() -> void:
 	current_dash_count += 1
+	# Keep recharing dashes if we are still missing dashes. 
+	if can_recharge_dash and current_dash_count < max_dash_count and dash_cooldown_timer.is_stopped():
+		dash_cooldown_timer.start()
 
 func _on_dash_duration_timer_timeout() -> void:
 	end_dash.emit()
 	dash_cooldown_timer.start()
 	can_dash = true
 	
+func _on_dash_recharge_delay_timer_timeout() -> void:
+	dash_cooldown_timer.start()
+	can_recharge_dash = true
